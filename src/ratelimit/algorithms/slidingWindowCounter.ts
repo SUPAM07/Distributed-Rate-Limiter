73263bbb9ec3fc7ae3da00a1ff9a310dcc40f99e
Supam Roy <supamroy8@gmail.com>
2026-03-09 18:19:32 +0530

    “fixed”

--- src/ratelimit/algorithms/slidingWindowCounter.ts
+++ src/ratelimit/algorithms/slidingWindowCounter.ts
@@ -21,7 +21,6 @@ export class SlidingWindowCounter implements RateLimitAlgorithm {
 
     let state: CounterState = await this.backend.get(key)
 
-    
     if (!state) {
       state = {
         currentWindowStart: now,
